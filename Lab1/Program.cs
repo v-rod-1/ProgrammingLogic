d630d8cb39c947174dc1ebbb17f5248e10b3d83a
v-rod-1 <v.rod216@yahoo.com>
2025-03-21 21:16:41 +0000

    Save before outputs

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,9 +1,28 @@
-﻿namespace Lab1;
+﻿using System.Text.Encodings.Web;
+
+namespace Lab1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Step 1 Code to print my name and course title using Console.WriteLine() 
+        Console.WriteLine("Roderick Vorhees IT-1050 - Lab 1");
+        
+        /* Name: Roderick Vorhees
+        Title: IT-1050 - Lab 1 */
+        
+        int favNum = 0; // Declaring my favorite number
+
+        string favLang = "Java"; // Declaring my favorite programming language (it's based on name)
+
+        double numProg = 1.5; // Declaring number of programs I have written
+
+        bool proExp = false; // Declaring if I have programming experience
+
+        const string schoolName = "Tri-C"; //Declaring a constant string for my school's name
+
+        
+
     }
 }
