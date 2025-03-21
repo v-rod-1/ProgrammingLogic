7198ceae8ec694fee6df9687532462ff9c6e8c03
v-rod-1 <v.rod216@yahoo.com>
2025-03-21 21:52:40 +0000

    Final product

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -12,9 +12,10 @@ class Program
         /* Name: Roderick Vorhees
         Title: IT-1050 - Lab 1 */
         
+        //Declaring variables below
         int favNum = 0; // Declaring my favorite number
 
-        string favLang = "Java"; // Declaring my favorite programming language (it's based on name)
+        string favLang = "Java"; // Declaring my favorite programming language
 
         double numProg = 1.5; // Declaring number of programs I have written
 
@@ -22,7 +23,16 @@ class Program
 
         const string schoolName = "Tri-C"; //Declaring a constant string for my school's name
 
-        
+        //Creating lines for output/print below
 
+        Console.WriteLine("My favorite number is " + favNum + "."); // Displaying output of my favorite number
+
+        Console.WriteLine("My favorite programming language is " + favLang + " because of its name."); // Displaying output for my favorite programming language
+
+        Console.WriteLine("I have written " + numProg + " programs. This one and another in IT 1025."); // Displaying output for the number of programs I have written
+
+        Console.WriteLine(proExp ? "I have programming experience." : "I have no programming experience."); // Displaying output for my programming experience
+
+        Console.WriteLine("The school that I am currently attending is " + schoolName + "."); // Displaying output of the school name
     }
 }
--- Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
+++ Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+475770224c3361047bd668458434ec0640cc5329")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5e6d6b7c27805bd77bc3b615da7fe9034f40817")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
