8f4052bfcad8537969911a03bf8b359c97bda7ee
v-rod-1 <v.rod216@yahoo.com>
2025-03-25 23:03:15 +0000

    Start of lab 2

--- Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
+++ Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5e6d6b7c27805bd77bc3b615da7fe9034f40817")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7198ceae8ec694fee6df9687532462ff9c6e8c03")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
