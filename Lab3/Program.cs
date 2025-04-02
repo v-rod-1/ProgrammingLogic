a1707b49d808fe55cb5ec9585225ade44d6b5e67
v-rod-1 <v.rod216@yahoo.com>
2025-04-02 17:32:59 +0000

    updating Lab3

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int a = 10
+        int b = 5
+        int c = 15
+        Command.WriteLine(a > b)
+        Command.WriteLine(a < c)
+        Command.WriteLine(a > b + c)
     }
 }
