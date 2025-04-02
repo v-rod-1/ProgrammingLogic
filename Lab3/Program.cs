6dffc0e9fed73c2bf9cf1ce4871a22766d7986c4
v-rod-1 <v.rod216@yahoo.com>
2025-04-02 21:26:05 +0000

    Completion of Lab3

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            // Part 1: Operator Practice
+            // Part 1: Operator practice
             // Declaring variables
             int a = 10;
             int b = 5;
@@ -16,7 +16,7 @@
             // Check if a < c
             Console.WriteLine($"a < c: {a < c}");
 
-            // If else statement to check if a > b and c
+            // If else statement to check if a > b && c
             if (a > b && a > c)
             {
                 Console.WriteLine("a > b && a > c: True");
@@ -29,7 +29,7 @@
             // Check if a > b or a > c using || in shorthand variation
             Console.WriteLine(a > b || a > c ? "a > b || a > c: True" : "a > b || a > c: False");
 
-            // Part 2: Boolean Logic
+            // Part 2: Boolean logic
             // Declaring variable
             bool isRaining = true;
             bool haveUmbrella = false;
@@ -44,6 +44,62 @@
                 Console.WriteLine("You're good to go!");
             }
 
+            // Part 3: Movie ticket pricing
+            // Asking the user to enter their age
+            Console.Write("Please enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());  // Read and convert the input to a integer variable
+
+            // Determining the ticket price category the user falls within using if... else statements 
+            if (age < 5) // ages younger than 5
+            {
+                Console.WriteLine("Ticket is free!");
+            }
+            else if (age >= 5 && age <= 12)
+            {
+                Console.WriteLine("Child ticket: $5");
+            }
+            else if (age >= 13 && age <= 64)
+            {
+                Console.WriteLine("Standard ticket: $10");
+            }
+            else // age 65 or older
+            {
+                Console.WriteLine("Senior ticket: $6");
+            }
+
+            // Part 4: Using a switch statement
+            // Asking the user to enter which day of the week it is using integers 1-7
+            Console.Write("Please enter the day of the week using (1-7) starting from monday: ");
+            int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+            // Using switch to display the corresponding day name
+            switch (dayNumber)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+                default:
+                    Console.WriteLine("Invalid day!");
+                    break;
+            }
         }
     }
 }
