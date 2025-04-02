e8a32854c349de06bdf9d8aecf06c49aab4313c6
v-rod-1 <v.rod216@yahoo.com>
2025-04-02 18:18:36 +0000

    Save for later

--- Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
+++ Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1707b49d808fe55cb5ec9585225ade44d6b5e67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f85e2d89ed01d14d03805d56bec9aa3c48e22caf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -1,14 +1,49 @@
-﻿namespace Lab3;
-
-class Program
+﻿namespace Lab3
 {
-    static void Main(string[] args)
+    class Program
     {
-        int a = 10
-        int b = 5
-        int c = 15
-        Command.WriteLine(a > b)
-        Command.WriteLine(a < c)
-        Command.WriteLine(a > b + c)
+        static void Main(string[] args)
+        {
+            // Part 1: Operator Practice
+            // Declaring variables
+            int a = 10;
+            int b = 5;
+            int c = 15;
+
+            // Check if a > b
+            Console.WriteLine($"a > b: {a > b}");
+
+            // Check if a < c
+            Console.WriteLine($"a < c: {a < c}");
+
+            // If else statement to check if a > b and c
+            if (a > b && a > c)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+
+            // Check if a > b or a > c using || in shorthand variation
+            Console.WriteLine(a > b || a > c ? "True" : "False");
+
+            // Part 2: Boolean Logic
+            // Declaring variables
+            bool isRaining = true;
+            bool haveUmbrella = false;
+
+            // If.. else statement. Using the ! operator to reverse the results of the true/false statement 
+            if (isRaining && !haveUmbrella)
+            {
+                Console.WriteLine("Take an umbrella!");
+            }
+            else
+            {
+                Console.WriteLine("You're good to go!");
+            }
+
+        }
     }
 }
