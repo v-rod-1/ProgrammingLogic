74f3eda7bc65ff733d4314a68a38b0b488da21a4
v-rod-1 <v.rod216@yahoo.com>
2025-05-09 19:40:04 +0000

    Lab 8

--- Lab7/obj/Debug/net8.0/Lab7.AssemblyInfo.cs
+++ Lab7/obj/Debug/net8.0/Lab7.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20f23dabb76a06ca9cc67b7e0a3bf4b51b237525")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b8405f636ec6d9963e8f5c79dc0f571aff26b3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Lab8/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Lab8;
+
+class Animal  // Parent class
+{
+  public virtual void animalSound() 
+  {
+    Console.WriteLine("The animal makes a sound");
+  }
+}
+
+class Cat : Animal  // Child class Cat 
+{
+  public override void animalSound() 
+  {
+    Console.WriteLine("The cat says: meow");
+  }
+}
+
+class Dog : Animal  // Child class Dog 
+{
+  public override void animalSound() 
+  {
+    Console.WriteLine("The dog says: bow wow");
+  }
+}
+
+class Program 
+{
+  static void Main(string[] args) 
+  {
+    Animal myAnimal = new Animal();  // Create a Animal object
+    Animal myCat = new Cat();  // Create a Cat object
+    Animal myDog = new Dog();  // Create a Dog object
+
+    myAnimal.animalSound();
+    myCat.animalSound();
+    myDog.animalSound();
+  }
+}
